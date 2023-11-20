1bc473a1a5703fdfcbdb45f9aaa0e88a348c0e90
AneshwarSalaria <aneshwarofficial@gmail.com>
2023-11-20 16:28:17 +0530

    Added Bio Scrapping tab

---
+++ Wiki_Web_Scrapping/project/WebApplication1/WebApplication1/Controllers/BioScrapController.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+// Additional libraries added to the codebase
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+using WebApplication1.Models;
+using System.Diagnostics;
+
+namespace WebApplication1.Controllers
+{
+    public class BioScrapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Output(FormDataModel formData)
+        {
+            // Process the form data, store it in a variable, or perform any other actions
+            string url = formData.InputData;
+
+            // Store the data in TempData to pass it back to the view
+            TempData["FormData"] = url;
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+
+
+            //var HeaderNames = doc.DocumentNode.SelectNodes("//div[@class='vector-toc-text']/text()");
+            //var titles = new List<OutputModel>();
+            //foreach (var item in HeaderNames)
+            //{
+            //titles.Add(new OutputModel { OutputData = item.InnerText.Trim() });
+            //}
+            //return View(titles);
+            
+            //string occupation = string.Empty;
+
+
+            string name = string.Empty;
+            string born = string.Empty; 
+            string almaMater = string.Empty; 
+            
+            //string works;
+            string children = string.Empty;
+            string spouse = string.Empty;
+            string parents = string.Empty;
+            string family = string.Empty;
+
+
+            HtmlNode divNode;
+            divNode = doc.DocumentNode.SelectSingleNode("//div[@class='fn']");
+            if (divNode != null)
+            {
+                name = divNode.InnerText.Trim();
+                divNode = null;
+            }
+
+            //divNode = doc.DocumentNode.SelectSingleNode("//td[@class='infobox-data role']");
+            //if (divNode != null)
+            //{
+            //    occupation = divNode.InnerText.Trim();
+            //    divNode = null;
+            //}
+
+            var labelNodes = doc.DocumentNode.SelectNodes("//th[@class='infobox-label']");
+
+            if (labelNodes != null)
+            {
+                foreach (var labelNode in labelNodes)
+                {
+                    // Get the text content of the label
+                    string label = labelNode.InnerText.Trim();
+
+                    // Find the corresponding data in the following sibling td element
+                    var dataNode = labelNode.SelectSingleNode("following-sibling::td[@class='infobox-data']");
+
+                    if (dataNode != null)
+                    {
+                        if (label == "Born")
+                        {
+                            born = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        }
+
+                        else if (label == "Alma&#160;mater")
+                        {
+                            almaMater = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        }
+
+                        //else if (label == "Occupation")
+                        //{
+                        //    occupation = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        //}
+
+                        //else if (label == "Works")
+                        //{
+                        //    works = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        //}
+
+                        else if (label == "Spouse")
+                        {
+                            spouse = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        }
+
+                        else if (label == "Children")
+                        {
+                            children = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        }
+
+                        else if (label == "Parent(s)")
+                        {
+                            parents = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        }
+
+                        else if (label == "Family")
+                        {
+                            family = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+                        }
+
+                        // Get the text content of the corresponding data
+                        // string data = HtmlEntity.DeEntitize(dataNode.InnerText).Trim();
+
+                        // Console.WriteLine($"{label}: {data}");
+                    }
+                }
+            }
+
+
+            BioOutputModel biodata = new BioOutputModel
+            {
+                Name = name,
+                Born = born,
+                AlmaMatter = almaMater,
+                Occupations = new List<string>(),
+                ChildrenCount = children,
+                Spouse = spouse,
+                Parents = parents,
+                Family = family,
+
+            };
+
+            var occupationNodes = doc.DocumentNode.SelectNodes("//td[@class='infobox-data role']//div[@class='hlist']//li");
+
+            if (occupationNodes != null)
+            {
+                foreach (var occupationNode in occupationNodes)
+                {
+                    string occupation = occupationNode.InnerText.Trim();
+                    biodata.Occupations.Add(occupation);
+                }
+            }
+
+
+
+            return View(biodata);
+        }
+    }
+    
+}
+    
+
+
+    
+
+       
+    
+
+
+
+            
---
+++ Wiki_Web_Scrapping/project/WebApplication1/WebApplication1/Models/BioOutputModel.cs
@@ -0,0 +1,44 @@
+﻿namespace WebApplication1.Models
+{
+    public class BioOutputModel
+    {
+        public string Name { get; set; }
+
+        public string? Born { get; set; }
+
+        public string? AlmaMatter { get; set; }
+
+        //public string? Occupation { get; set; }
+        public List<String>? Occupations{ get; set; }
+        
+        //public int? ChildrenCount { get; set; }
+
+        public string? ChildrenCount { get; set; }
+
+        public string? Spouse { get; set; }
+
+        public string? Parents { get; set; }
+
+        public string? Family { get; set; }
+
+
+        //public List<Spouse>? Spouses { get; set; }
+
+        //public List<Parent>? Parents { get; set; }
+
+
+    }
+
+    //public class Spouse
+    //{
+    //    public string Name { get; set; }
+    //    public int MarriedYear { get; set; }
+    //    public int? DivorcedYear { get; set; }
+    //}
+
+    //public class Parent
+    //{
+    //    public string Name { get; set; }
+    //}
+
+}
